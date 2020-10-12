928c61f8587a93e1af7325d6d66420965c7f8a0e
Lucinda Davies <71652362+LucindaDavies@users.noreply.github.com>
2020-10-12 19:05:49 +0100

    Add files via upload

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+answer = 10
+
+print("Guess a number between 1 and 10")
+guess = int(input())
+
+if guess == 10:
+    print("That is the correct number!!")
+else:
+    print("Sorry that is not the correct number")
